d38a4140989e9775459c7695d17ac26548dad480
maxlero <Maxlero@yandex.ru>
2018-03-11 10:51:01 +0300

    Entry point added Beans inspection added Logger added

--- src/main/java/by/grsu/orel/Starter.java
+++ src/main/java/by/grsu/orel/Starter.java
@@ -1,5 +1,6 @@
 package by.grsu.orel;
 
+import org.apache.log4j.Logger;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,9 +11,10 @@ import java.util.Arrays;
 
 @SpringBootApplication
 public class Starter {
+    private static final Logger logger = Logger.getLogger(Starter.class);
 
     public static void main(String[] args) {
-
+        logger.info("Logger says: 'Hello!'");
         SpringApplication.run(Starter.class, args);
 
     }
