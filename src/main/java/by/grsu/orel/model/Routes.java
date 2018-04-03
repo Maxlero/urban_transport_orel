ecaea6681abba69d3b15cc6edbe70ac4eb4fbbb2
maxlero <maxlero@yandex.ru>
2018-04-03 12:45:37 +0300

    routes info can be represented on the page /routes from mysql

---
+++ src/main/java/by/grsu/orel/model/Routes.java
@@ -0,0 +1,51 @@
+package by.grsu.orel.model;
+
+import com.google.gson.Gson;
+
+import javax.persistence.*;
+
+@Entity
+public class Routes {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private Long id;
+
+	private String name;
+	private String route;
+
+	public Route[] getRoutes() {
+		Gson gson = new Gson();
+
+		return gson.fromJson(route, Route[].class);
+	}
+
+	@Override
+	public String toString() {
+		return "Routes [id=" + id + ", name=" + name + ", route=" + route + "]";
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getRoute() {
+		return route;
+	}
+
+	public void setRoute(String route) {
+		this.route = route;
+	}
+}
