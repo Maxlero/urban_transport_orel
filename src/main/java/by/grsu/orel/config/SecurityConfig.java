c344a459ebc1f7c2e29152da52a0b0e39f6f2136
maxlero <maxlero@yandex.ru>
2018-04-05 17:24:20 +0300

    template for add route page added

--- src/main/java/by/grsu/orel/config/SecurityConfig.java
+++ src/main/java/by/grsu/orel/config/SecurityConfig.java
@@ -24,6 +24,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 						"/",
 						"/registration",
 						"/routes",
+						"/add",
 						"/js/**",
 						"/css/**",
 						"/img/**",
