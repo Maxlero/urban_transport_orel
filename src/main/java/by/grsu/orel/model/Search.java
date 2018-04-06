2f897890b496d80241821c4428339d84ceb1987f
maxlero <maxlero@yandex.ru>
2018-04-06 18:19:00 +0300

    bluffing commit

---
+++ src/main/java/by/grsu/orel/model/Search.java
@@ -0,0 +1,22 @@
+package by.grsu.orel.model;
+
+public class Search {
+	private String pName;
+	private String lName;
+
+	public String getpName() {
+		return pName;
+	}
+
+	public void setpName(String pName) {
+		this.pName = pName;
+	}
+
+	public String getlName() {
+		return lName;
+	}
+
+	public void setlName(String lName) {
+		this.lName = lName;
+	}
+}
